789a7da2cdf4db947413ad95b39d33d4d4f205d2
Suhail Rehman <suhailrehman@gmail.com>
2022-06-24 10:29:28 -0500

    First stab at Lazo Arrow Server

---
+++ src/main/java/lazo/arrowserver/flightrpc/Dataset.java
@@ -0,0 +1,27 @@
+package lazo.arrowserver.flightrpc;
+
+import org.apache.arrow.vector.ipc.message.ArrowRecordBatch;
+import org.apache.arrow.vector.types.pojo.Schema;
+
+import java.util.List;
+
+
+public class Dataset {
+    private final List<ArrowRecordBatch> batches;
+    private final Schema schema;
+    private final long rows;
+    public Dataset(List<ArrowRecordBatch> batches, Schema schema, long rows) {
+        this.batches = batches;
+        this.schema = schema;
+        this.rows = rows;
+    }
+    public List<ArrowRecordBatch> getBatches() {
+        return batches;
+    }
+    public Schema getSchema() {
+        return schema;
+    }
+    public long getRows() {
+        return rows;
+    }
+}
