a143d1220f13ee7238e097004747873863acf3e0
Suhail Rehman <suhailrehman@gmail.com>
2022-07-26 21:13:55 -0500

    Updated Build and Server Scripts

--- run_arrowserver.sh
+++ run_arrowserver.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
 ./gradlew clean
-./gradlew uberJAR
-java -jar build/libs/lazo-0.1.0-uber.jar --add-opens=java.base/java.nio=ALL-UNNAMED
+./gradlew build
+./gradlew shadowJar #--warning-mode all --stacktrace
+
+java -jar build/libs/lazo-0.1.0-all.jar --add-opens=java.base/java.nio=ALL-UNNAMED &
+server_pid=$!
+echo $server_pid > /tank/local/suhail/relic-datalake/scripts/server.pid
+#java -jar build/libs/lazo-0.1.0-uber.jar --add-opens=java.base/java.nio=ALL-UNNAMED
